fce69ef2649bf282a4c73d59c46bc0439c84f5b9
Mirraely <mirraelymoura@gmail.com>
2025-03-20 22:22:25 -0300

    Adiciona conteúdo a serviços e depoimentos.

--- components/sections/Servicos.vue
+++ components/sections/Servicos.vue
@@ -3,89 +3,32 @@
     <h1 class="text-[2rem] text-center text-[#ffd700] title-hero pt-6 pb-12">
       Nossos Serviços
     </h1>
-    <div class="flex items-center justify-center gap-4">
-      <div class="lado-a">
-        <div class="flex items-center bg-white rounded-md shadow-md p-4 mb-4">
-          <img
-            src="https://via.placeholder.com/80"
-            alt="Imagem 1"
-            class="w-1/3 mr-4 rounded-md"
-          />
-          <div class="w-2/3">
-            <h2 class="text-xl font-semibold mb-2">Título do Card 1</h2>
-            <p class="text-gray-600">
-              Descrição breve do card 1. Pode conter algumas linhas de texto.
-            </p>
+    <div class="flex flex-wrap items-center justify-center gap-4">
+      <div
+        v-for="(service, index) in services"
+        :key="service.id"
+        :class="{
+          'w-full sm:w-1/2 md:w-1/3 lg:w-1/4': true,
+        }"
+      >
+        <div
+          class="flex flex-col bg-white rounded-md shadow-md p-4 mb-4 h-full"
+        >
+          <div class="flex justify-end">
+            <Tag
+              :value="service.categoria"
+              :class="tagColor(service.categoria)"
+              class="mb-2"
+            />
           </div>
-        </div>
-        <div class="flex items-center bg-white rounded-md shadow-md p-4">
           <img
-            src="https://via.placeholder.com/80"
-            alt="Imagem 2"
-            class="w-1/3 mr-4 rounded-md"
+            :src="service.photo"
+            :alt="service.serviço"
+            class="w-full rounded-md mb-4"
           />
-          <div class="w-2/3">
-            <h2 class="text-xl font-semibold mb-2">Título do Card 2</h2>
-            <p class="text-gray-600">
-              Descrição breve do card 2. Pode conter algumas linhas de texto.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="lado-b">
-        <div class="flex items-center bg-white rounded-md shadow-md p-4 mb-4">
-          <img
-            src="https://via.placeholder.com/80"
-            alt="Imagem 3"
-            class="w-1/3 mr-4 rounded-md"
-          />
-          <div class="w-2/3">
-            <h2 class="text-xl font-semibold mb-2">Título do Card 3</h2>
-            <p class="text-gray-600">
-              Descrição breve do card 3. Pode conter algumas linhas de texto.
-            </p>
-          </div>
-        </div>
-        <div class="flex items-center bg-white rounded-md shadow-md p-4">
-          <img
-            src="https://via.placeholder.com/80"
-            alt="Imagem 4"
-            class="w-1/3 mr-4 rounded-md"
-          />
-          <div class="w-2/3">
-            <h2 class="text-xl font-semibold mb-2">Título do Card 4</h2>
-            <p class="text-gray-600">
-              Descrição breve do card 4. Pode conter algumas linhas de texto.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="lado-c">
-        <div class="flex items-center bg-white rounded-md shadow-md p-4 mb-4">
-          <img
-            src="https://via.placeholder.com/80"
-            alt="Imagem 3"
-            class="w-1/3 mr-4 rounded-md"
-          />
-          <div class="w-2/3">
-            <h2 class="text-xl font-semibold mb-2">Título do Card 3</h2>
-            <p class="text-gray-600">
-              Descrição breve do card 3. Pode conter algumas linhas de texto.
-            </p>
-          </div>
-        </div>
-        <div class="flex items-center bg-white rounded-md shadow-md p-4">
-          <img
-            src="https://via.placeholder.com/80"
-            alt="Imagem 4"
-            class="w-1/3 mr-4 rounded-md"
-          />
-          <div class="w-2/3">
-            <h2 class="text-xl font-semibold mb-2">Título do Card 4</h2>
-            <p class="text-gray-600">
-              Descrição breve do card 4. Pode conter algumas linhas de texto.
-            </p>
+          <div class="flex-grow">
+            <h2 class="text-xl font-semibold mb-2">{{ service.serviço }}</h2>
+            <p class="text-gray-600 text-justify">{{ service.descrição }}</p>
           </div>
         </div>
       </div>
@@ -93,6 +36,26 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import services from "../../assets/db/servicos.json";
+import Tag from "primevue/tag";
+
+function tagColor(categoria) {
+  switch (categoria) {
+    case "Cabelo":
+      return "p-tag-info"; // Azul claro
+    case "Maquiagem":
+      return "p-tag-success"; // Verde
+    case "Manicure":
+      return "p-tag-warning"; // Amarelo
+    case "Unhas":
+      return "p-tag-danger"; // Vermelho
+    case "Estética":
+      return "p-tag-secondary"; // Cinza
+    default:
+      return "p-tag-primary"; // Azul padrão
+  }
+}
+</script>
 
 <style scoped></style>
