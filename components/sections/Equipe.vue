4b282552def8355c0f94a65757902481bd0fe18a
Mirraely <mirraelymoura@gmail.com>
2025-03-22 00:52:24 -0300

    Ajusta responsividade de equipe.

--- components/sections/Equipe.vue
+++ components/sections/Equipe.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="pt-8 pb-18 px-16 md:px-24 bg-black flex items-center flex-col justify-center"
+    class="pt-8 pb-18 px-4 md:px-24 bg-black flex items-center flex-col justify-center"
     id="equipe"
   >
-    <h1 class="text-[2rem] text-center text-[#ffd700] title-hero pt-6 pb-6">
+    <h1 class="text-2xl text-center text-[#ffd700] title-hero pt-6 pb-6">
       Conheça nosso time
     </h1>
     <div class="text-start text-white flex flex-col gap-10">
-      <span
+      <span class="w-full text-[14px] md:text-[1rem]"
         ><p class="indent-[40px] text-justify mb-2.5">
           No coração do nosso salão, pulsa uma equipe de artistas da beleza,
           apaixonados por realçar o que há de mais belo em você. Somos um time
@@ -19,12 +19,12 @@
           se transformar.
         </p>
         <p
-          class="text-center text-[#ffd600] font-semibold text-sm/6 md:text-sm/9 md:text-[1.2rem] text-[1rem]"
+          class="text-start sm:text-center text-[#ffd600] font-semibold text-sm/6 md:text-sm/9 md:text-[1.2rem] text-[1rem]"
         >
           Conheça o Time dos Sonhos que Transforma Beleza em Arte!
         </p>
       </span>
-      <div class="w-150 m-auto">
+      <div class="max-w-150 m-auto">
         <img
           src="../../public/testemonialsImages/teste-image-woman.jpg"
           alt="imagem da equipe"
