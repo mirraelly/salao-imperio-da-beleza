9f4e90a7a0f3b5407fd1ca3a75026632866f8b7a
Mirraely <mirraelymoura@gmail.com>
2025-03-21 22:51:22 -0300

    Ajusta tela de sobre.

--- components/sections/Sobre.vue
+++ components/sections/Sobre.vue
@@ -1,66 +1,70 @@
 <template>
-  <div class="py-8 px-4 sm:px-24 bg-black" id="sobre">
+  <div class="py-8 px-4 md:px-0 lg:px-8 bg-black" id="sobre">
     <h1 class="text-[2rem] text-center text-[#ffd700] title-hero">
       Quem Somos
     </h1>
-    <div
-      class="flex flex-col-reverse sm:flex-row w-full items-start justify-evenly gap-16 mt-8"
-    >
-      <div class="w-full sm:w-1/2 flex flex-col gap-6">
-        <div>
-          <h2
-            class="text-start text-[#ffd600] font-semibold text-sm/9 md:text-[1.5rem] text-[1rem] max-w-[35rem]"
-          >
-            Mais do que um salão, <br />
-            <span> um espaço para você se sentir incrível.</span>
-          </h2>
-        </div>
-        <div class="w-full text-start text-white">
-          <span class="w-full">
-            <p class="indent-[40px] text-justify mb-2.5">
-              Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé, cursus
-              quis, vehicula ac nisi. Nulla id gravida magna, ut semper sapien.
-              Posuere libero varius. Nullam a nisl ut ante blandit hendrerit.
-              Aenean sit amet nisi. Diuretics paradis num copo é motivis de
-              denguis.
-            </p>
-            <p class="indent-[40px] text-justify mb-2.5">
-              Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé, cursus
-              quis, vehicula ac nisi. Nulla id gravida magna, ut semper sapien.
-              Posuere libero varius. Nullam a nisl ut ante blandit hendrerit.
-              Aenean sit amet nisi. Diuretics paradis num copo é motivis de
-              denguis.
-            </p>
-            <p class="indent-[40px] text-justify mb-2.5">
-              Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé, cursus
-              quis, vehicula ac nisi. Nulla id gravida magna, ut semper sapien.
-              Posuere libero varius. Nullam a nisl ut ante blandit hendrerit.
-              Aenean sit amet nisi. Diuretics paradis num copo é motivis de
-              denguis.
-            </p>
-          </span>
-        </div>
-        <div class="w-[12rem] h-[2.5rem] flex">
-          <p class="text-[2rem] text-[#ffd600] assinatura">Rita de Kácia</p>
-        </div>
-      </div>
-      <div class="w-1/3">
-        <div class="container w-full">
-          <div
-            class="custom-card relative bg-[#808080] p-[20px] text-center rounded-lg w-full overflow-hidden"
-          >
-            <img
-              src="../../assets/images/rita-depe.jpeg"
-              alt="Mulher pensativa"
-              class="profile-image w-full rounded-lg object-cover"
-            />
-            <div
-              class="absolute top-0 right-[-1px] w-[117px] h-[126px] border-t-[8px] border-r-[9px] border-t-[#ffc107] border-r-[#ffc107] rounded-bl-full rounded-tl-none rounded-br-none rounded-tr-none bg-transparent"
-            ></div>
-            <div
-              class="name-tag absolute bg-[#ffc107] w-full text-black text-center font-bold bottom-0 left-0 py-[10px] px-0 text-[1.1rem]"
+    <div>
+      <div
+        class="flex flex-col-reverse xl:flex-row items-center lg:items-stretch sm:flex-row w-full justify-center gap-8 lg:gap-20 mt-8 h-min max-w-[80rem]"
+      >
+        <div class="w-full md:w-1/2 flex flex-col gap-6 ladoA">
+          <div>
+            <h2
+              class="text-start text-[#ffd600] font-semibold text-sm/6 md:text-sm/9 md:text-[1.5rem] text-[1.2rem] max-w-[35rem]"
             >
-              Cabeleireira a mais de x anos
+              Mais do que um salão, <br />
+              <span> um espaço para você se sentir incrível.</span>
+            </h2>
+          </div>
+          <div class="w-full text-start text-white">
+            <span class="w-full text-[14px] md:text-[1rem]">
+              <p class="indent-[40px] text-justify mb-2.5">
+                Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé,
+                cursus quis, vehicula ac nisi. Nulla id gravida magna, ut semper
+                sapien. Posuere libero varius. Nullam a nisl ut ante blandit
+                hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é
+                motivis de denguis.
+              </p>
+              <p class="indent-[40px] text-justify mb-2.5">
+                Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé,
+                cursus quis, vehicula ac nisi. Nulla id gravida magna, ut semper
+                sapien. Posuere libero varius. Nullam a nisl ut ante blandit
+                hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é
+                motivis de denguis.
+              </p>
+              <p class="indent-[40px] text-justify mb-2.5">
+                Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé,
+                cursus quis, vehicula ac nisi. Nulla id gravida magna, ut semper
+                sapien. Posuere libero varius. Nullam a nisl ut ante blandit
+                hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é
+                motivis de denguis.
+              </p>
+            </span>
+          </div>
+          <div class="w-[12rem] h-[2.5rem] flex">
+            <p class="text-[1.5rem] md:text-[2rem] text-[#ffd600] assinatura">
+              Rita de Kácia
+            </p>
+          </div>
+        </div>
+        <div class="w-full max-w-[20rem] md:w-1/3 ladoA">
+          <div class="container w-full h-full">
+            <div
+              class="custom-card h-full relative bg-[#808080] p-[20px] text-center rounded-lg w-full overflow-hidden"
+            >
+              <img
+                src="../../assets/images/rita-depe.jpeg"
+                alt="Mulher pensativa"
+                class="profile-image w-full h-full rounded-lg object-cover"
+              />
+              <div
+                class="absolute top-0 right-[-1px] w-[117px] h-[126px] border-t-[8px] border-r-[9px] border-t-[#ffc107] border-r-[#ffc107] rounded-bl-full rounded-tl-none rounded-br-none rounded-tr-none bg-transparent"
+              ></div>
+              <div
+                class="name-tag absolute bg-[#ffc107] w-full text-black text-center font-bold bottom-0 left-0 py-[10px] px-0 text-[14px] md:text-[1.1rem]"
+              >
+                Cabeleireira a mais de x anos
+              </div>
             </div>
           </div>
         </div>
